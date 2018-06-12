6f0d25a9da228f5ffbf18556d654577e0b43474b
forose <forose@qq.com>
2018-06-13 04:52:25 +0800

    Initial commit

---
+++ src/main/java/com/sammery/IndexAction.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sammery;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Date;
+import java.util.Map;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
+
+/**
+ * 
+ */
+@Conversion()
+public class IndexAction extends ActionSupport {
+    
+    private Date now = new Date(System.currentTimeMillis());
+    private Map map;
+    
+    @TypeConversion(converter = "com.sammery.DateConverter")
+    public Date getDateNow() { return now; }
+    
+    public String execute() throws Exception {
+        now = new Date(System.currentTimeMillis());
+        return SUCCESS;
+    }
+}
